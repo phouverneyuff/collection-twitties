5bd1bf9485591ccd603ece3591391086b7631b66
phouverneyuff <ph.ouverne@gmail.com>
2019-01-07 07:15:20 -0200

    init

---
+++ collector/src/main/java/com/processoitau/collector/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.processoitau.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.social.twitter.api.Twitter;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
