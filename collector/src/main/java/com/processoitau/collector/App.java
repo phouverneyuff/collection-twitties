e131b91bcd3664cf936cb6b9746553e77ad732a4
Paulo Henrique Ouverney <pouverne@everis.com>
2019-01-07 22:45:17 -0200

    Criando dockerfile e melhorias

--- collector/src/main/java/com/processoitau/collector/App.java
+++ collector/src/main/java/com/processoitau/collector/App.java
@@ -2,7 +2,6 @@ package com.processoitau.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.social.twitter.api.Twitter;
 
 @SpringBootApplication
 public class App {
