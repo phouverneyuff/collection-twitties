a0090e0112e995c2c4963481659d35545526cf16
Paulo Henrique Ouverney <pouverne@everis.com>
2019-01-10 12:13:54 -0200

    Melhorias no docker

--- docker-compose-push-up.sh
+++ docker-compose-push-up.sh
@@ -5,4 +5,4 @@ docker-compose -f docker-compose.yml build --no-cache
 
 docker-compose -f docker-compose.yml push
 
-docker-compose -f docker-compose.yml up            
+docker-compose -f docker-compose.yml up -d
